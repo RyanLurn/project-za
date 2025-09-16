092c6516d5546551c1c38f04b2bcc8c0d13f73db
Ryan Luong <lumintriet@gmail.com>
2025-09-16 08:17:09 +0700

    Add dark mode.

---
+++ src/components/providers/index.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "@/components/providers/theme";
+
+function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {children}
+    </ThemeProvider>
+  );
+}
+
+export { Providers };
