f2c35138b7b9b15bf9f5165f81d4662fa56a63da
Ryan Luong <lumintriet@gmail.com>
2025-09-16 08:41:19 +0700

    Add deps and eslint plugin for tanstack query.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
 import js from "@eslint/js";
+import tanStackQuery from "@tanstack/eslint-plugin-query";
 import tanStackRouter from "@tanstack/eslint-plugin-router";
 import prettier from "eslint-config-prettier";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(["dist/"]),
   {
     files: ["**/*.{ts,tsx}"],
@@ -23,6 +24,7 @@ export default tseslint.config([
     }
   },
   ...tanStackRouter.configs["flat/recommended"],
+  ...tanStackQuery.configs["flat/recommended"],
   {
     name: "prettier",
     ...prettier
