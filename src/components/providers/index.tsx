37c4b4fc3ed9dfbc0393e4bf726d634bb010db76
Ryan Luong <lumintriet@gmail.com>
2025-09-16 08:22:46 +0700

    Add mode toggle and toast components.

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from "@/components/providers/theme";
+import { Toaster } from "@/components/ui/sonner";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       {children}
+      <Toaster closeButton richColors position="top-center" />
     </ThemeProvider>
   );
 }
