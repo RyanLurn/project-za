e5b58771caf2dd2079ed350e6233205ff7ba0d42
Ryan Luong <lumintriet@gmail.com>
2025-09-16 20:46:24 +0700

    Prompt component and send message mutation.

--- src/features/chat/components/prompt/index.tsx
+++ src/features/chat/components/prompt/index.tsx
@@ -1,24 +1,51 @@
-import { useState } from "react";
-import { useMutation } from "@tanstack/react-query";
+import { type FormEvent, useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 import { PromptEditor } from "@/features/chat/components/prompt/editor";
 import { SendButton } from "@/features/chat/components/prompt/send-button";
+import { createMessage } from "@/features/chat/functions/create-message";
+import { useSurrealClient } from "@/hooks/use-surreal";
 
 function Prompt() {
+  const queryClient = useQueryClient();
+  const surrealClient = useSurrealClient();
   const [prompt, setPrompt] = useState("");
-  const {} = useMutation();
+  const { mutate, isPending } = useMutation({
+    mutationFn: createMessage,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["messages"]
+      });
+    },
+    onError: err => {
+      console.error(err);
+      toast.error("Failed to send message");
+    }
+  });
 
   function changePrompt(prompt: string) {
     setPrompt(prompt);
   }
 
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    mutate({
+      surrealClient,
+      role: "user",
+      text: prompt
+    });
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <PromptEditor
         prompt={prompt}
         changePrompt={changePrompt}
-        isDisabled={isDisabled}
+        isDisabled={isPending}
       />
-      <SendButton isDisabled={isDisabled} />
+      <SendButton isDisabled={isPending} />
     </form>
   );
 }
+
+export { Prompt };
